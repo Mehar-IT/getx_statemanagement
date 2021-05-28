51c75d1078f234870140bca81f76d71b1fcc4c4f
Mehar-IT <hamzatariquearain@gmail.com>
2021-05-28 16:30:34 +0500

    formated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:test1/controller/dataController.dart';
-
-import 'pages/detailScreen.dart';
+import 'pages/listscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,29 +26,5 @@ class MyData extends StatelessWidget {
       ),
       body: MyList(),
     );
-  }
-}
-
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetX<DataController>(
-        init: DataController(),
-        builder: (obj) => ListView.builder(
-              itemCount: obj.student.length,
-              itemBuilder: (context, index) => Card(
-                child: ListTile(
-                  onTap: () {
-                    Get.to(() => StudentDetail(
-                          student: obj.student[index],
-                        ));
-                  },
-                  leading: Text('${obj.student[index].id}'),
-                  title: Text('${obj.student[index].name}'),
-                  subtitle: Text(
-                      '${obj.student[index].dept},${obj.student[index].uni}'),
-                ),
-              ),
-            ));
   }
 }
---
+++ lib/pages/listscreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:test1/controller/dataController.dart';
+import 'package:get/get.dart';
+
+import 'detailScreen.dart';
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetX<DataController>(
+        init: DataController(),
+        builder: (obj) => ListView.builder(
+              itemCount: obj.student.length,
+              itemBuilder: (context, index) => Card(
+                child: ListTile(
+                  onTap: () {
+                    Get.to(() => StudentDetail(
+                          student: obj.student[index],
+                        ));
+                  },
+                  leading: Text('${obj.student[index].id}'),
+                  title: Text('${obj.student[index].name}'),
+                  subtitle: Text(
+                      '${obj.student[index].dept},${obj.student[index].uni}'),
+                ),
+              ),
+            ));
+  }
+}
